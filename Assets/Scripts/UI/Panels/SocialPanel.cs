4fd391d025ede98a466dbd274068c54d9b0d5366
Madoww <fszafran2b1@gmail.com>
2021-07-12 18:34:17 +0200

    Changes to Social Panel. Folder management.

--- Assets/Scripts/UI/Panels/SocialPanel.cs
+++ Assets/Scripts/UI/Panels/SocialPanel.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class SocialPanel : MonoBehaviour
 {
     [SerializeField] private SocialSong socialSongPrefab;
+    [SerializeField] private Transform root;
 
     private void OnEnable()
     {
         foreach(Song song in GameManager.Instance.songs)
         {
-            SocialSong socialSong = Instantiate(socialSongPrefab, transform);
+            SocialSong socialSong = Instantiate(socialSongPrefab, root);
             socialSong.title.SetText(song.name);
             socialSong.genre.SetText(song.genre.genreName);
         }
